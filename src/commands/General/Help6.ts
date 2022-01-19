d9806f14f76365b892c7b264a35be588d202d07c
HiRo HaMaDa <94060412+HIROHAMADA22@users.noreply.github.com>
2022-01-19 13:22:15 +0530

    Update Help6.ts

--- src/commands/General/Help6.ts
+++ src/commands/General/Help6.ts
@@ -8,7 +8,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'help6',
+            command: 'help',
             description: 'Displays the help menu or shows the info of the command provided',
             category: 'general',
             usage: `${client.config.prefix}help6 (command_name)`,
